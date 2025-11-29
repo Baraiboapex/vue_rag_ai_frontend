8cdc4e5a18ca7584bd78ce5651e991e47fcb3afc
matth <matth@BARAIBOBOX>
2025-11-28 22:23:31 -0500

    fixed an issue with deployment

--- src/App.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-  import AIPrompt from "./components/AIPrompt.vue";
-</script>
-
-<template>
-  <AIPrompt msg="RAG AI Example" />
-</template>
--- src/components/RagPromptLoading.vue
+++
@@ -1,5 +0,0 @@
-<script>
-    import {ref} from "vue";
-</script>
-<template>
-</template>
